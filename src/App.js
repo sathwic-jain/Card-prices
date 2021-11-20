b746a44e0aee702ff66300ab3af96cdf09392629
Sathwic Jain <sathwic.j@gmail.com>
2021-11-21 03:00:35 +0530

    Card prices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-12">
+              <div class="box my-5 mx-5 mx-lg-2 p-3">
+                <p class="top">FREE</p>
+                <p class="text-center"><span class="heading">$0</span><span>/month</span></p>
+      
+                <hr/>
+      
+                <ul class="tick">
+                  <li>Single User</li>
+                  <li>5GB Storage</li>
+                  <li>Unlimited Public Projects</li>
+                  <li>Community Access</li>
+                </ul>
+      
+                <ul class="cross">
+                  <li>Unlimited Private Projects</li>
+                  <li>Dedicated Phone Support</li>
+                  <li>Free Subdomain</li>
+                  <li>Monthly Status Reports</li>
+                </ul>
+                
+                <button class="button" >Button</button>
+                
+              </div>
+            </div>
+      
+            <div class="col-lg-4 col-12">
+              <div class="box my-5 mx-5 mx-lg-2 p-3">
+                <p class="top">PLUS</p>
+                <p class="text-center"><span class="heading">$9</span><span>/month</span></p>
+      
+                <hr/>
+      
+                <ul class="tick">
+                  <li>Single User</li>
+                  <li>5GB Storage</li>
+                  <li>Unlimited Public Projects</li>
+                  <li>Community Access</li>
+                  <li>Unlimited Private Projects</li>
+                  <li>Dedicated Phone Support</li>
+                  <li>Free Subdomain</li>
+                </ul>
+      
+                <ul class="cross">
+                  <li>Monthly Status Reports</li>
+                </ul>
+                <button class="button" >Button</button>
+              </div>
+            </div>
+      
+            <div class="col-lg-4 col-12">
+              <div class="box my-5 mx-5 mx-lg-2 p-3">
+                <p class="top">PRO</p>
+                <p class="text-center"><span class="heading">$49</span><span>/month</span></p>
+      
+                <hr/>
+      
+                <ul class="tick" >
+                  <li>Single User</li>
+                  <li>5GB Storage</li>
+                  <li>Unlimited Public Projects</li>
+                  <li>Community Access</li>
+                  <li>Unlimited Private Projects</li>
+                  <li>Dedicated Phone Support</li>
+                  <li>Free Subdomain</li>
+                  <li>Monthly Status Reports</li>
+                </ul>
+                <p></p>
+                <button class="button" >Button</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
 
 export default App;
+
